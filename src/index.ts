a930328ba63bbb1b98a7846971a6a6d896814a36
Phathdt <phathdt379@gmail.com>
2023-12-26 22:37:06 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,52 @@
-// src/index.ts
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import { Api, TelegramClient } from 'telegram'
+import { StringSession } from 'telegram/sessions'
 
-/*
- * Load up and parse configuration details from
- * the `.env` file to the `process.env`
- * object of Node.js
- */
 dotenv.config()
 
-/*
- * Create an Express application and get the
- * value of the PORT environment variable
- * from the `process.env`
- */
 const app: Express = express()
 const port = process.env.PORT || 3000
 
-/* Define a route for the root path ("/")
- using the HTTP GET method */
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server')
+const apiId = parseInt(process.env.API_ID || '0')
+const apiHash = process.env.API_HASH || ''
+const botToken = process.env.BOT_TOKEN || ''
+
+const stringSession = new StringSession('')
+const client = new TelegramClient(stringSession, apiId, apiHash, {
+  connectionRetries: 5,
 })
 
-/* Start the Express app and listen
- for incoming requests on the specified port */
+;(async () => {
+  await client.start({
+    botAuthToken: botToken,
+  })
+
+  console.log('You should now be connected.')
+
+  console.log(client.session.save())
+})()
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ msg: 'pong' })
+})
+
+app.get('/users', async (req: Request, res: Response) => {
+  const username = String(req.query.username)
+
+  try {
+    const result = await client.invoke(
+      new Api.users.GetFullUser({
+        id: username,
+      })
+    )
+
+    res.json({ msg: result })
+  } catch (error) {
+    res.status(500).json({ error: error })
+  }
+})
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
 })
